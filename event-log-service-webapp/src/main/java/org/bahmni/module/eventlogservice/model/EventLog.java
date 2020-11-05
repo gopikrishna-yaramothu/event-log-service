fec5a5f67ea047cd96c1972ef0a3f7a089690fe0
gopikrishna-yaramothu <gopikrishna.y@thoughtworks.com>
2020-11-05 15:24:02 +0530

    Added Static Filters with range from Filter1 to Filter10 in EventLog Model& Added Same in MIgrations also
    Co-Authored-By: Shri Ram <26056117+shriram-k@users.noreply.github.com>

--- event-log-service-webapp/src/main/java/org/bahmni/module/eventlogservice/model/EventLog.java
+++ event-log-service-webapp/src/main/java/org/bahmni/module/eventlogservice/model/EventLog.java
@@ -34,14 +34,35 @@ public class EventLog {
     @Column(name = "parent_uuid")
     private String parentUuid;
 
-    @Column(name = "province")
-    private String province;
+    @Column(name = "filter1")
+    private String filter1;
 
-    @Column(name = "district")
-    private String district;
+    @Column(name = "filter2")
+    private String filter2;
 
-    @Column(name = "facility")
-    private String facility;
+    @Column(name = "filter3")
+    private String filter3;
+
+    @Column(name = "filter4")
+    private String filter4;
+
+    @Column(name = "filter5")
+    private String filter5;
+
+    @Column(name = "filter6")
+    private String filter6;
+
+    @Column(name = "filter7")
+    private String filter7;
+
+    @Column(name = "filter8")
+    private String filter8;
+
+    @Column(name = "filter9")
+    private String filter9;
+
+    @Column(name = "filter10")
+    private String filter10;
 
     public EventLog(String uuid, Date timestamp, String object, String category, String filter, String parentUuid) {
         this.uuid = uuid;
@@ -52,16 +73,24 @@ public class EventLog {
         this.parentUuid = parentUuid;
     }
 
-    public EventLog(String uuid, Date timestamp, String object, String category, String filter, String parentUuid, String province, String district, String facility) {
+    public EventLog(String uuid, Date timestamp, String object, String category, String filter, String parentUuid, String province, String district, String facility,
+                    String filter1, String filter2, String filter3, String filter4, String filter5, String filter6, String filter7, String filter8, String filter9, String filter10) {
         this.uuid = uuid;
         this.timestamp = timestamp;
         this.object = object;
         this.category = category;
         this.filter = filter;
         this.parentUuid = parentUuid;
-        this.province = province;
-        this.district = district;
-        this.facility = facility;
+        this.filter1 = filter1;
+        this.filter2 = filter2;
+        this.filter3 = filter3;
+        this.filter4 = filter4;
+        this.filter5 = filter5;
+        this.filter6 = filter6;
+        this.filter7 = filter7;
+        this.filter8 = filter8;
+        this.filter9 = filter9;
+        this.filter10 = filter10;
     }
 
     public EventLog() {
@@ -119,27 +148,83 @@ public class EventLog {
         return parentUuid;
     }
 
-    public String getProvince() {
-        return province;
+    public String getFilter1() {
+        return filter1;
     }
 
-    public void setProvince(String province) {
-        this.province = province;
+    public void setFilter1(String filter1) {
+        this.filter1 = filter1;
     }
 
-    public String getDistrict() {
-        return district;
+    public String getFilter2() {
+        return filter2;
     }
 
-    public void setDistrict(String district) {
-        this.district = district;
+    public void setFilter2(String filter2) {
+        this.filter2 = filter2;
     }
 
-    public String getFacility() {
-        return facility;
+    public String getFilter3() {
+        return filter3;
     }
 
-    public void setFacility(String facility) {
-        this.facility = facility;
+    public void setFilter3(String filter3) {
+        this.filter3 = filter3;
+    }
+
+    public String getFilter4() {
+        return filter4;
+    }
+
+    public void setFilter4(String filter4) {
+        this.filter4 = filter4;
+    }
+
+    public String getFilter5() {
+        return filter5;
+    }
+
+    public void setFilter5(String filter5) {
+        this.filter5 = filter5;
+    }
+
+    public String getFilter6() {
+        return filter6;
+    }
+
+    public void setFilter6(String filter6) {
+        this.filter6 = filter6;
+    }
+
+    public String getFilter7() {
+        return filter7;
+    }
+
+    public void setFilter7(String filter7) {
+        this.filter7 = filter7;
+    }
+
+    public String getFilter8() {
+        return filter8;
+    }
+
+    public void setFilter8(String filter8) {
+        this.filter8 = filter8;
+    }
+
+    public String getFilter9() {
+        return filter9;
+    }
+
+    public void setFilter9(String filter9) {
+        this.filter9 = filter9;
+    }
+
+    public String getFilter10() {
+        return filter10;
+    }
+
+    public void setFilter10(String filter10) {
+        this.filter10 = filter10;
     }
 }
